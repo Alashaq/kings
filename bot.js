7f4d1c3c2f2866e5ea45ebdf12f4b8c15e9912f3
Alashaq <40842773+Alashaq@users.noreply.github.com>
2018-09-29 21:07:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '$'
+const prefix = '1'
 client.on('ready', () => {
   console.log('======================================')
   console.log(`Logged in as ${client.user.tag}!`);
@@ -81,4 +81,4 @@ if (message.content === "سبام" + "<@" + `${client.user.id}` + ">") {
 });
 
 
-client.login('NDgyMDAxNDU3Njg4NjA4Nzg4.Do5cCw.tb63yHlA0xIhmqxQbUDTA8jw5m0');
+client.login('NDk0OTQ5ODA3NzgyMTAxMDAz.DpFdIA.tpr0wIQPhQSfe-u1RwkjmGaWrWE');
